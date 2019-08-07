8a254c44e84376a948f0c9927c1ceb80b957822b
flux <ramazanov.null@gmail.com>
2019-08-07 17:17:24 +0300

    start create authorization

--- DjangoNoobs/authorization/urls.py
+++ DjangoNoobs/authorization/urls.py
@@ -13,14 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls import url
-from . import views
+
+from django.urls import path
+
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('admin/', admin.site.urls),
-    url(r'^accounts/', include('django.contrib.auth.urls')),
-    path('testing/', include('testsystem.urls'))
+    #path('', , name=''),
 ]
+
---
+++ DjangoNoobs/authorization/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+import hashlib
+
+
+
+# Create your views here.
