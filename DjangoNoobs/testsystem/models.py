62a31bd15254c0f784cc80062bca6adc6bb68f59
AntonAks <antonaks84@gmail.com>
2019-08-11 22:39:10 +0300

    add some docstrings to model

--- DjangoNoobs/testsystem/models.py
+++ DjangoNoobs/testsystem/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 
-QUESTION_TYPES = [('Single Answer','Single Answer'),('Multi Answer','Multi Answer'),('User Input','User Input')]
+QUESTION_TYPES = [('Single Answer', 'Single Answer'),
+                  ('Multi Answer', 'Multi Answer'),
+                  ('User Input', 'User Input')]
 
 class QuizCategories(models.Model):
+    """
+    Class for define quiz category 
+    """
     category_name = models.CharField(max_length=50, unique=True)
-    
+
     class Meta:
         verbose_name_plural = 'Quiz Categories'
         ordering = ['category_name']
@@ -13,6 +18,10 @@ class QuizCategories(models.Model):
         return f'{self.category_name}'
 
 class Quiz(models.Model):
+    """
+    Class for define high level of quiz structure
+    Linked to CategoryQuiz model
+    """
     name = models.CharField(max_length=50, null=False)
     description = models.CharField(max_length=300, null=False)
     category_name = models.ForeignKey(QuizCategories, on_delete=models.SET_DEFAULT, default=None)
@@ -25,17 +34,24 @@ class Quiz(models.Model):
         return f'{self.name} ({self.category_name})'
 
 class QuizQuestion(models.Model):
+    """
+    Class for define question for quiz
+    Linked to Quiz model
+    """
     question_text = models.TextField(null=False)
     question_code = models.TextField(null=True)
-    question_type = models.CharField(max_length= 20, null=True, choices=QUESTION_TYPES, default='Single Answer')
+    question_type = models.CharField(max_length=20, null=True, choices=QUESTION_TYPES, default='Single Answer')
     question_points = models.IntegerField(null=False, default=1)
     linked_quiz = models.ForeignKey(Quiz, on_delete=models.PROTECT, null=False)
 
     def __str__(self):
         return f'{self.question_text[:30]}...'
 
-
 class QuizAnswers(models.Model):
+    """
+    Class for define answers for question(s)
+    Linked to Quiz Question model
+    """
     summary = models.TextField(null=False)
     correct_flag = models.BooleanField()
     linked_question = models.ForeignKey(QuizQuestion, on_delete=models.PROTECT, null=False)
@@ -44,8 +60,12 @@ class QuizAnswers(models.Model):
         return f'{self.linked_question} ({self.correct_flag})'
 
 class ActiveQuiz(models.Model):
-    name = models.CharField(max_length= 50, null=False)
-    category_name = models.CharField(max_length= 50, null=False)
+    """
+    Class for define status of quiz
+    Linked to Quiz Question model
+    """
+    name = models.CharField(max_length=50, null=False)
+    category_name = models.CharField(max_length=50, null=False)
     question = models.ForeignKey(QuizQuestion, on_delete=models.SET_DEFAULT, default=None) 
     question_done_flag = models.BooleanField(null=True)
     correct_answer_flag = models.BooleanField(null=True)
