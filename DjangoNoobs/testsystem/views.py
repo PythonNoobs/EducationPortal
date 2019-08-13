08aca7c73c9a259922cc9ac6c0499f3ceac20eac
AntonAks <antonaks84@gmail.com>
2019-08-13 13:32:36 +0300

    minor changes for views and template

--- DjangoNoobs/testsystem/views.py
+++ DjangoNoobs/testsystem/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render, HttpResponse
-
+from django.shortcuts import render, get_object_or_404, HttpResponse
+from .models import Quiz, QuizQuestion, QuizAnswers
 
 # Create your views here.
-def test_view(request):
-    return render(request, 'testsystem/test_index.html')
+def quiz_view(request):
+
+    quiz_data = [item for item in Quiz.objects.all()]
+    page_content = {'quiz_data': quiz_data}
+
+    return render(request, 'testsystem/test_index.html', context=page_content)
+
+
+def quiz_details(request, quiz_id):
+    quiz_questions = QuizQuestion.objects().filter(linked_quiz=quiz_id)
+    print(quiz_questions)
+    return render(request, 'testsystem/questions.html')
